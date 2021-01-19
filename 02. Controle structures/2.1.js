a63390596bef95bad3c3572ca0e04bf082e9d100
Nadja-BeCode <nadja.agostinho@gmail.com>
2021-01-19 10:02:05 +0100

    add exercise

---
+++ 02. Controle structures/2.1.js
@@ -0,0 +1,10 @@
+let adult = 47;
+let child = 13;
+
+if ((adult >= 21)) || (child >= 6)) {
+    console.log("You're an adult");
+   } else {
+    console.log("You're a child");
+   }
+
+   //BACK TO FIXED THAT
