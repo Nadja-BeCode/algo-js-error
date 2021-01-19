80981b80cfd30551c8d77fb9ec2fedf77382fac1
Nadja-BeCode <nadja.agostinho@gmail.com>
2021-01-19 09:28:03 +0100

    last modif

--- 1.8.js
+++ 1.8.js
@@ -1,27 +1,35 @@
-const readline = require("readline-sync")
-let name = readline . question ("W?");
-console . log("Three questions left")
-let firstName = readline . question ("And your first name?");
-console . log("Three questions left")
-let city = readline . question ("Where do you live?");
-console . log("Three questions left")
-console.log( "My name is " + name + " and my first name is " + firstName + " I live in " + city);
-console . log("Three questions left")
+const readline = require("readline-sync");
+let weird = readline . question ("What weird food combinations do you really enjoy??");
+console.log("Eight questions left");
 
-console . log("Three questions left")
+let odd = readline . question ("What odd smell do you really enjoy?");
+console.log("Seven questions left");
+
+let movie = readline . question ("What movie can you watch over and over without ever getting tired of?");
+console.log("Six questions left");
+
+let deco = readline . question ("Where do you get most of the decorations for your home?");
+console.log("Five questions left");
+
+let old = readline . question ("What “old person” things do you do?");
+console.log("Four questions left");
+
+let joke = readline . question ("What’s the funniest joke you know by heart?");
+console.log("Three questions left");
+
+let fun = readline . question ("What’s the funniest actual name you’ve heard of someone having?");
+console.log("Two questions left");
+
+let strange = readline . question ("What country is the strangest?");
+console.log("One question lef");
+
+let noise = readline . question ("What’s the most annoying noise?");
+console.log("No more questions! The final result is...");
 
 
-console . log("Three questions left")
 
-console . log("Three questions left")
 
-console . log("Three questions left")
-
-console . log("Three questions left")
-
-console . log("Three questions left")
-
-console . log("Three questions left")
-
-console . log("Three questions left")
-
+console.log( "The weird food combinations I do really enjoy is " + weird + " is the smell of " + odd + 
+" Maybe " + movie + " I get most of them from " + deco + " I think it's " + old +
+" I think this one is good " + joke + " Actually " + fun +
+" let me guess " + strange + " it will be " + noise);
